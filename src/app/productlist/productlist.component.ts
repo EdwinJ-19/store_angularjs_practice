f5718499c5772fdc040f2f9da96953b26891ff80
EdwinJ-19 <edwinjames2001@gmail.com>
2024-08-03 08:03:24 +0530

    modified_product_components

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -8,21 +8,88 @@ import { Product } from './product';
 })
 export class ProductlistComponent implements OnInit {
 
-  product1:Product={
+  electronicsProducts : Product[] = [
+  {
     name:"Samsung S23+",
     price: 80000,
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     category:"Mobile Electronics",
     image:"../assets/images/samsung_s23+.jpg"
-  }
-
-  product2:Product={
-    name:"TShirt",
-    price: 500,
+  },
+  {
+    name:"Iphone 15 Pro Max",
+    price: 120000,
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    category:"Clothes",
-    image:"../assets/images/tshirt.webp"
+    category:"Mobile Electronics",
+    image:"../assets/images/iphone15.jpg"
+  },
+  {
+    name:"Redmi 11T Pro",
+    price: 40000,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category:"Mobile Electronics",
+    image:"../assets/images/redmi_11t_pro.webp"
+  },
+  {
+    name:"Vivo V23",
+    price: 20000,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category:"Mobile Electronics",
+    image:"../assets/images/vivo_v23.jpg"
   }
+  ];
+
+  clothesProducts : Product[] = [
+    {
+      name:"TShirt",
+      price: 500,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Clothes",
+      image:"../assets/images/tshirt.webp"
+    },
+    {
+      name:"Pants",
+      price: 300,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Clothes",
+      image:"../assets/images/pant.webp"
+    },
+    {
+      name:"Belt",
+      price: 100,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Clothes",
+      image:"../assets/images/belt.webp"
+    },
+    {
+      name:"Spectacles",
+      price: 500,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Clothes",
+      image:"../assets/images/spectacles.jpg"
+    },
+
+  ];
+
+  category1 = "Mobile Electronics";
+  category2 = "Clothes";
+
+
+  // product1:Product={
+  //   name:"Samsung S23+",
+  //   price: 80000,
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   category:"Mobile Electronics",
+  //   image:"../assets/images/samsung_s23+.jpg"
+  // }
+
+  // product2:Product={
+  //   name:"TShirt",
+  //   price: 500,
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   category:"Clothes",
+  //   image:"../assets/images/tshirt.webp"
+  // }
   constructor() { }
 
   ngOnInit(): void {
