905dc79967c4fd43546c6a2577c9c2034846b39e
EdwinJ-19 <edwinjames2001@gmail.com>
2024-08-11 14:48:49 +0530

    changed_product_list_to_main_app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './productlist/product';
 
 @Component({
   selector: 'app-root',
@@ -18,4 +19,75 @@ export class AppComponent {
     // console.log(event.target.value); //the target means to target the input field and values shows the values of the input inside itself.
     this.searchProduct = event.target.value;
   }
+
+  electronicsProducts : Product[] = [
+    {
+      name:"Samsung S23+",
+      price: 80000,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Mobile Electronics",
+      image:"../assets/images/samsung_s23+.jpg",
+      stock:"In Stock"
+    },
+    {
+      name:"Iphone 15 Pro Max",
+      price: 120000,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Mobile Electronics",
+      image:"../assets/images/iphone15.jpg",
+      stock:"Not In Stock"
+    },
+    {
+      name:"Redmi 11T Pro",
+      price: 40000,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Mobile Electronics",
+      image:"../assets/images/redmi_11t_pro.webp",
+      stock:"In Stock"
+    },
+    {
+      name:"Vivo V23",
+      price: 20000,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category:"Mobile Electronics",
+      image:"../assets/images/vivo_v23.jpg",
+      stock:"Not In Stock"
+    }
+    ];
+  
+    // clothesProducts : Product[] = [
+    //   {
+    //     name:"TShirt",
+    //     price: 500,
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //     category:"Clothes",
+    //     image:"../assets/images/tshirt.webp",
+    //     stock:"In Stock"
+    //   },
+    //   {
+    //     name:"Pants",
+    //     price: 300,
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //     category:"Clothes",
+    //     image:"../assets/images/pant.webp",
+    //     stock:"In Stock"
+    //   },
+    //   {
+    //     name:"Belt",
+    //     price: 100,
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //     category:"Clothes",
+    //     image:"../assets/images/belt.webp",
+    //     stock:"Not In Stock"
+    //   },
+    //   {
+    //     name:"Spectacles",
+    //     price: 500,
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //     category:"Clothes",
+    //     image:"../assets/images/spectacles.jpg",
+    //     stock:"In Stock"
+    //   },
+  
+    // ];
 }
--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from './product';
 
 @Component({
@@ -20,76 +20,80 @@ export class ProductlistComponent implements OnInit {
     stock:"",
   }
 
-  electronicsProducts : Product[] = [
-  {
-    name:"Samsung S23+",
-    price: 80000,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    category:"Mobile Electronics",
-    image:"../assets/images/samsung_s23+.jpg",
-    stock:"In Stock"
-  },
-  {
-    name:"Iphone 15 Pro Max",
-    price: 120000,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    category:"Mobile Electronics",
-    image:"../assets/images/iphone15.jpg",
-    stock:"Not In Stock"
-  },
-  {
-    name:"Redmi 11T Pro",
-    price: 40000,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    category:"Mobile Electronics",
-    image:"../assets/images/redmi_11t_pro.webp",
-    stock:"In Stock"
-  },
-  {
-    name:"Vivo V23",
-    price: 20000,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    category:"Mobile Electronics",
-    image:"../assets/images/vivo_v23.jpg",
-    stock:"Not In Stock"
-  }
-  ];
+  @Input() products !: Product[];
+  clothesProducts: any;
+  allProducts !: Product[];
 
-  clothesProducts : Product[] = [
-    {
-      name:"TShirt",
-      price: 500,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      category:"Clothes",
-      image:"../assets/images/tshirt.webp",
-      stock:"In Stock"
-    },
-    {
-      name:"Pants",
-      price: 300,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      category:"Clothes",
-      image:"../assets/images/pant.webp",
-      stock:"In Stock"
-    },
-    {
-      name:"Belt",
-      price: 100,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      category:"Clothes",
-      image:"../assets/images/belt.webp",
-      stock:"Not In Stock"
-    },
-    {
-      name:"Spectacles",
-      price: 500,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      category:"Clothes",
-      image:"../assets/images/spectacles.jpg",
-      stock:"In Stock"
-    },
+  // electronicsProducts : Product[] = [
+  // {
+  //   name:"Samsung S23+",
+  //   price: 80000,
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   category:"Mobile Electronics",
+  //   image:"../assets/images/samsung_s23+.jpg",
+  //   stock:"In Stock"
+  // },
+  // {
+  //   name:"Iphone 15 Pro Max",
+  //   price: 120000,
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   category:"Mobile Electronics",
+  //   image:"../assets/images/iphone15.jpg",
+  //   stock:"Not In Stock"
+  // },
+  // {
+  //   name:"Redmi 11T Pro",
+  //   price: 40000,
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   category:"Mobile Electronics",
+  //   image:"../assets/images/redmi_11t_pro.webp",
+  //   stock:"In Stock"
+  // },
+  // {
+  //   name:"Vivo V23",
+  //   price: 20000,
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   category:"Mobile Electronics",
+  //   image:"../assets/images/vivo_v23.jpg",
+  //   stock:"Not In Stock"
+  // }
+  // ];
 
-  ];
+  // clothesProducts : Product[] = [
+  //   {
+  //     name:"TShirt",
+  //     price: 500,
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     category:"Clothes",
+  //     image:"../assets/images/tshirt.webp",
+  //     stock:"In Stock"
+  //   },
+  //   {
+  //     name:"Pants",
+  //     price: 300,
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     category:"Clothes",
+  //     image:"../assets/images/pant.webp",
+  //     stock:"In Stock"
+  //   },
+  //   {
+  //     name:"Belt",
+  //     price: 100,
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     category:"Clothes",
+  //     image:"../assets/images/belt.webp",
+  //     stock:"Not In Stock"
+  //   },
+  //   {
+  //     name:"Spectacles",
+  //     price: 500,
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     category:"Clothes",
+  //     image:"../assets/images/spectacles.jpg",
+  //     stock:"In Stock"
+  //   },
+
+  // ];
 
   category1 = "Mobile Electronics";
   category2 = "Clothes";
@@ -135,11 +139,14 @@ export class ProductlistComponent implements OnInit {
   //   image:"../assets/images/tshirt.webp"
   // }
   constructor() {
-    this.tempValue = this.electronicsProducts;
-    this.tempValue1 =  this.clothesProducts;
+    // this.tempValue = this.electronicsProducts;
+    // this.tempValue1 =  this.clothesProducts;
    }
 
   ngOnInit(): void {
+    // console.log(this.products);
+    this.allProducts = this.products;
+    this.tempValue = this.allProducts;
   }
 
   // allProducts = this.electronicsProducts.concat(this.clothesProducts);
@@ -148,12 +155,12 @@ export class ProductlistComponent implements OnInit {
     const typedValue = event.target.value;
     // const typedValue = args[0];
 
-    this.tempValue = this.electronicsProducts.filter((prod)=>{
-      return prod.name.toLowerCase().includes(typedValue.toLowerCase());
-    })
-    this.tempValue1 = this.clothesProducts.filter((prod)=>{
-      return prod.name.toLowerCase().includes(typedValue.toLowerCase());
-    })
+    // this.tempValue = this.electronicsProducts.filter((prod)=>{
+    //   return prod.name.toLowerCase().includes(typedValue.toLowerCase());
+    // })
+    // this.tempValue1 = this.clothesProducts.filter((prod)=>{
+    //   return prod.name.toLowerCase().includes(typedValue.toLowerCase());
+    // })
   }
 
   onClickSubmitElectronics(){
